b24bf99393caa0f6d06515b52323b8147f6572ad
dzou <dzou@company.com>
2019-04-04 18:20:08 +0530

    Hackerrank solutions(1,2)

---
+++ Python_If-Else.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+if(N%2==0):
+    if(2<=N<=5):
+        print("Not Weird")
+
+    elif(6<=N<=20):
+        print("Weird")
+    
+    elif(N>20):
+        print("Not Weird")
+
+else:
+    print("Weird")
+
